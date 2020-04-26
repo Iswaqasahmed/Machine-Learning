4ca990ebd50212c82f8d4457214e8c6a70fe45cd
waqas ahmed <WA541964@gmail.com>
2020-04-27 02:49:30 +0500

    row and column shapes added

--- Bostan_House.py
+++ Bostan_House.py
@@ -4,3 +4,17 @@ from keras.datasets import boston_housing
 (x_train,y_train),(x_test,y_test) = boston_housing.load_data() 
 print("the type of x_train data is ",type(x_train))
 print("the type of y_train data is ",type(y_train))
+print("###########################################")
+print("First Way ")
+
+print("The shape of x_train data ",x_train.shape)
+print("The shape of y_train data ",y_train.shape)
+print("The shape of x_test data ",x_test.shape)
+print("The shape of y_test data ",y_test.shape)
+print("###########################################")
+print("Second Way ")
+
+print("The row of x_train {0} and column of x_train {1} ".format(x_train.shape[0],x_train.shape[1]))
+print("The row of y_train {0} and column of y_train 1 ".format(y_train.shape[0]))
+print("The row of x_test {0} and column of x_test {1} ".format(x_test.shape[0],x_test.shape[1]))
+print("The row of y_test {0} and column of y_test 1 ".format(y_test.shape[0]))
